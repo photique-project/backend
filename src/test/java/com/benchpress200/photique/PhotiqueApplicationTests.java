d1f3bce580ab97f99455100408b0fa38723df6a8
BenchPress200 <jms0538@naver.com>
2026-03-01 23:46:30 +0900

    test: 스프링 컨텍스트 통합 실행 코드 제외

--- src/test/java/com/benchpress200/photique/PhotiqueApplicationTests.java
+++ src/test/java/com/benchpress200/photique/PhotiqueApplicationTests.java
@@ -1,10 +1,10 @@
 package com.benchpress200.photique;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("local")
+@Disabled("단위 테스트만 진행")
 @SpringBootTest
 class PhotiqueApplicationTests {
 
