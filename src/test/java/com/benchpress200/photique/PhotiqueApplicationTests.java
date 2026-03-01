ec3d9d49be8e569d0083b8f6aa92701414691cc9
BenchPress200 <jms0538@naver.com>
2026-03-01 23:30:18 +0900

    test: test 코드 실행 워크플로우 추가

--- src/test/java/com/benchpress200/photique/PhotiqueApplicationTests.java
+++ src/test/java/com/benchpress200/photique/PhotiqueApplicationTests.java
@@ -2,7 +2,9 @@ package com.benchpress200.photique;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class PhotiqueApplicationTests {
 
