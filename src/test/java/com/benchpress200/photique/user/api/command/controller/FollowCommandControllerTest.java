a9369f4e3b923b895b28d7ae7a774c5550db31a5
BenchPress200 <jms0538@naver.com>
2026-03-16 21:49:07 +0900

    test: 팔로우 취소 API 컨트롤러 테스트 코드 작성

--- src/test/java/com/benchpress200/photique/user/api/command/controller/FollowCommandControllerTest.java
+++ src/test/java/com/benchpress200/photique/user/api/command/controller/FollowCommandControllerTest.java
@@ -2,6 +2,7 @@ package com.benchpress200.photique.user.api.command.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,7 +62,39 @@ public class FollowCommandControllerTest extends BaseControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @Test
+    @DisplayName("팔로우 취소 요청 시 요청이 유효하면 204를 반환한다")
+    public void unfollow_whenRequestIsValid() throws Exception {
+        // given
+        doNothing().when(unfollowUseCase).unfollow(any());
+
+        // when
+        ResultActions resultActions = requestUnfollow("1");
+
+        // then
+        resultActions
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("팔로우 취소 요청 시 followeeId가 숫자가 아니면 400을 반환한다")
+    public void unfollow_whenFolloweeIdIsNotNumber() throws Exception {
+        // given
+        doNothing().when(unfollowUseCase).unfollow(any());
+
+        // when
+        ResultActions resultActions = requestUnfollow("invalid");
+
+        // then
+        resultActions
+                .andExpect(status().isBadRequest());
+    }
+
     private ResultActions requestFollow(String followeeId) throws Exception {
         return mockMvc.perform(post(ApiPath.FOLLOW_ROOT, followeeId));
+    }
+
+    private ResultActions requestUnfollow(String followeeId) throws Exception {
+        return mockMvc.perform(delete(ApiPath.FOLLOW_ROOT, followeeId));
     }
 }
