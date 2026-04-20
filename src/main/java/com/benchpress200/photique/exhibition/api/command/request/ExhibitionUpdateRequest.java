3ca651e250886e0760d1d12610009ecd1e87e036
BenchPress200 <jms0538@naver.com>
2026-04-20 22:09:34 +0900

    test(exhibition): 전시회 수정 API 통합 테스트 코드 작성

--- src/main/java/com/benchpress200/photique/exhibition/api/command/request/ExhibitionUpdateRequest.java
+++ src/main/java/com/benchpress200/photique/exhibition/api/command/request/ExhibitionUpdateRequest.java
@@ -10,8 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ExhibitionUpdateRequest {
     private static final String INVALID_TITLE = "Invalid title";
