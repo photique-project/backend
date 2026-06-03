5dc01f0642af404be2662cd7619c7618fdd2523c
BenchPress200 <jms0538@naver.com>
2026-06-03 20:37:26 +0900

    test: 테스트 코드 병렬 처리 프로파일링 설정 추가

--- src/test/java/com/benchpress200/photique/support/base/BaseIntegrationTest.java
+++ src/test/java/com/benchpress200/photique/support/base/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import com.benchpress200.photique.config.TestDatabaseCleanerConfig;
 import com.benchpress200.photique.constant.Profile;
 import com.benchpress200.photique.support.util.DatabaseCleaner;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 @ActiveProfiles(Profile.TEST)
 @Import(TestDatabaseCleanerConfig.class)
+@Tag("integration")
 public abstract class BaseIntegrationTest extends BaseTestContainerTest {
     @Autowired
     protected MockMvc mockMvc;
